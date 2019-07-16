58cffaf8207c689bb25785380433168c25f0d719
Seo Jun Lee <52553554+lsj1329@users.noreply.github.com>
2019-07-16 10:36:34 +0900

    Add files via upload

---
+++ Argparse_test_voronoi.py
@@ -0,0 +1,50 @@
+import argparse
+from PIL import Image
+import random
+import math
+
+parser = argparse.ArgumentParser()
+parser.add_argument('X',type=int,help="You to decide how many times to draw")
+parser.add_argument('D',type=str,help="Draw the picture and run the file")
+args = parser.parse_args()
+X = args.X
+D = args.D
+
+def generate_voronoi_diagram(width, height, num_cells):
+  image = Image.new("RGB", (width, height))
+  putpixel = image.putpixel
+  imgx, imgy = image.size
+  nx = []
+  ny = []
+  nr = []
+  ng = []
+  nb = []
+  for i in range(num_cells):
+    nx.append(random.randrange(imgx))
+    ny.append(random.randrange(imgy))
+    nr.append(random.randrange(256))
+    ng.append(random.randrange(256))
+    nb.append(random.randrange(256))
+
+  for y in range(imgy):
+    for x in range(imgx):
+      dmin = math.hypot(imgx, imgy)
+      j=-1
+
+      for i in range(num_cells):
+
+        d = math.hypot(nx[i]-x, ny[i]-y)
+        if d < dmin:
+          dmin = d
+          j = i
+      putpixel((x, y), (nr[j], ng[j], nb[j]))
+
+  image.save("Voronoi.png", "PNG")
+  print("\t\t < Finished drawing!! > \n\t[ Just a moment, please ]\n")
+  if D != '':
+    image.show()
+
+
+
+if __name__=="__main__":
+  generate_voronoi_diagram(500, 500, X)
